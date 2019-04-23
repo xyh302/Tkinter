0e098790460967a3cf7f06d1c81a351031c30f11
xyh302a <490453102@qq.com>
2019-04-23 16:46:57 +0800

    change

--- createNumber.py
+++ createNumber.py
@@ -3,11 +3,7 @@ from tkinter import *
 from tkinter import ttk
 import os
 
-<<<<<<< HEAD
 path = os.getcwd() + "\\number.txt"
-=======
-path = os.getcwd()+"\\number.txt"
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 
 alist = []
 
@@ -15,11 +11,7 @@ for i in range(10):
     for j in range(i, 10):
         for k in range(j, 10):
             for l in range(k, 10):
-<<<<<<< HEAD
                 num = (str(i) + str(j) + str(k) + str(l))
-=======
-                num = (str(i)+str(j)+str(k)+str(l))
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
                 alist.append(num)
 
 
@@ -43,24 +35,15 @@ def callCheckbutton2():
 def callCheckbutton3():
     alist2 = []
     for eachNumber in alist:
-<<<<<<< HEAD
         for i, ele in enumerate(eachNumber):
             if i > 0 and ele == eachNumber[i - 1]:
-=======
-        for i,ele in enumerate(eachNumber):
-            if i > 0 and ele == eachNumber[i-1]:
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
                 if eachNumber not in alist2:
                     alist2.append(eachNumber)
                 continue
     for i in alist2:
         alist.remove(i)
     print(alist)
-<<<<<<< HEAD
 
-=======
-           
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 
 def callCheckbutton4(event):
     var = comb.get()
@@ -70,7 +53,6 @@ def callCheckbutton4(event):
             alist2.append(i)
     for i in alist2:
         alist.remove(i)
-<<<<<<< HEAD
     print(alist, var, "callCheckbutton4")
 
 
@@ -105,9 +87,6 @@ def callCheckbutton4_4(event):
     for i in alist2:
         alist.remove(i)
     print(alist, var, "callCheckbutton4_4")
-=======
-    print(alist,"callCheckbutton4")
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 
 
 def callCheckbutton5(event):
@@ -117,7 +96,6 @@ def callCheckbutton5(event):
     for i in alist:
         if var in i:
             alist2.append(i)
-<<<<<<< HEAD
     print(alist2, var, "callCheckbutton5")
     alist = alist2
 
@@ -141,9 +119,6 @@ def callCheckbutton5_3(event):
         if var in i:
             alist2.append(i)
     print(alist2, var, "callCheckbutton5_3")
-=======
-    print(alist2,"callCheckbutton5")
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
     alist = alist2
 
 
@@ -152,15 +127,9 @@ def callCheckbutton6(event):
     var = comb6.get()
     alist2 = []
     for i in alist:
-<<<<<<< HEAD
         if int(i[0]) + int(i[1]) + int(i[2]) + int(i[3]) < int(var):
             alist2.append(i)
     print(alist2, "callCheckbutton6")
-=======
-        if int(i[0])+int(i[1])+int(i[2])+int(i[3]) < int(var ):
-            alist2.append(i)
-    print(alist2,"callCheckbutton6")
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
     alist = alist2
 
 
@@ -169,15 +138,9 @@ def callCheckbutton7(event):
     var = comb7.get()
     alist2 = []
     for i in alist:
-<<<<<<< HEAD
         if int(i[0]) + int(i[1]) + int(i[2]) + int(i[3]) > int(var):
             alist2.append(i)
     print(alist2, "callCheckbutton7")
-=======
-        if int(i[0])+int(i[1])+int(i[2])+int(i[3]) > int(var):
-            alist2.append(i)
-    print(alist2,"callCheckbutton7")
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
     alist = alist2
 
 
@@ -191,7 +154,6 @@ def callCheckbutton8(event):
             alist2.append(i)
     for i in alist2:
         alist.remove(i)
-<<<<<<< HEAD
     print(alist, "callCheckbutton8")
 
 
@@ -248,14 +210,10 @@ def callCheckbutton8_7(event):
 #             alist2.append(i)
 #     alist = alist2
 #     print(alist, "callCheckbutton9")
-=======
-    print(alist,"callCheckbutton8")
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 
 
 def callCheckbutton9(event):
     global alist
-<<<<<<< HEAD
     a = comb9.get()
     b = comb9_2.get()
     c = comb9_3.get()
@@ -270,15 +228,6 @@ def callCheckbutton9(event):
             alist2.append(i)
     alist = alist2
     print(alist, "callCheckbutton9")
-=======
-    var = comb9.get()
-    alist2 = []
-    for i in alist:
-        if var in i:
-            alist2.append(i)
-    print(alist2,"alist")
-    alist = alist2
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 
 
 def run1():
@@ -286,12 +235,8 @@ def run1():
         for i in alist:
             f.write(i)
             f.write("\r\n")
-<<<<<<< HEAD
 
 
-=======
- 
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 root = Tk()
 root.title('数字生成器')
 
@@ -303,17 +248,12 @@ vlang.set(1)  # 第一个组初始值为1
 vos.set(2)  # 第一个组初始值为2
 # 创建两个组，不同的组，各个按钮互不影响。
 Label(root, text="为避免发生错误，以下9个条件，请从上到下进行选择").grid()
-<<<<<<< HEAD
-=======
-Label(root, text="第一项默认必选，第2-9项不填默认不选").grid()
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
 Label(root, text="").grid()
 Checkbutton(root, text="1.A(0...9)/B(0...9)C(0...9)D(0...9);(四个数都是0-9中选择;)", command=callCheckbutton1).grid()
 Checkbutton(root, text="2.A=B不等于C、D;(只允许两个数相同)", command=callCheckbutton2).grid()
 Checkbutton(root, text="3.A不等于B不等于C不等于D(四个数不同)", command=callCheckbutton3).grid()
 
 Label(root, text="4.A\B\C\D排除掉一个数,请输入你要排除的数字：").grid()
-<<<<<<< HEAD
 comb = ttk.Combobox(root, values=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 comb.grid()
 comb.bind("<<ComboboxSelected>>", callCheckbutton4)
@@ -394,44 +334,3 @@ comb9_3.bind("<<ComboboxSelected>>", callCheckbutton9)
 Button(root, text='点击查看结果', command=run1).grid()
 
 root.mainloop()
-=======
-comb = ttk.Combobox(root,values=[0,1,2,3,4,5,6,7,8,9])
-comb.grid()
-comb.bind("<<ComboboxSelected>>",callCheckbutton4)
-
-Label(root, text="5.A\B\C\D中必有一个数,请输入必有的数字：").grid()
-comb5 = ttk.Combobox(root,values=[0,1,2,3,4,5,6,7,8,9])
-comb5.grid()
-comb5.bind("<<ComboboxSelected>>",callCheckbutton5)
-
-Label(root, text="6.四个数之和小于多少，请输入你希望小于的数字：").grid()
-comb6 = ttk.Combobox(root,values=[x for x in range(37)])
-comb6.grid()
-comb6.bind("<<ComboboxSelected>>",callCheckbutton6)
-
-Label(root, text="7.四个数之和大于多少，请输入你希望大于的数字：").grid()
-comb7 = ttk.Combobox(root,values=[x for x in range(37)])
-comb7.grid()
-comb7.bind("<<ComboboxSelected>>",callCheckbutton7)
-
-Label(root, text="8.A\B\C\D不能同时存在的两个数，请输入你选择的第一个数：").grid()
-comb8 = ttk.Combobox(root,values=[0,1,2,3,4,5,6,7,8,9])
-comb8.grid()
-#comb8.bind("<<ComboboxSelected>>",callCheckbutton8)
-comb8_2 = ttk.Combobox(root,values=[0,1,2,3,4,5,6,7,8,9])
-comb8_2.grid()
-comb8_2.bind("<<ComboboxSelected>>",callCheckbutton8)
-
-Label(root, text="9.A\B\C\D中存在(1、2、3)三者之一,请输入第一个数：").grid()
-comb9 = ttk.Combobox(root,values=[1,2,3])
-comb9.grid()
-comb9.bind("<<ComboboxSelected>>",callCheckbutton9)
-
-Button(root, text='确定(点击确定查看结果)', command=run1).grid()
-lbl = Label(root,text='结果写入桌面的number.txt')
-lbl.grid()
-
- 
-root.mainloop()
-
->>>>>>> c6b2705248e01ba8b57349b629a0831de1983b0e
